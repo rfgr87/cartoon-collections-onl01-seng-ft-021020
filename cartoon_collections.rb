3667027ecde497fe6b33e1db6a3b763389c291f7
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-12 19:12:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,11 +19,21 @@ end
 
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  while i < array.length
+    if array[i].length > 4
+      return true
+    else
+      i += 1
+    end
+  end
+    return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"])
+  i = 0
+  while i < array.length
+    if array[i] ==
 end
