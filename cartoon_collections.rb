5f5f3923bde3c4cca2239aa5bbf865941b04ba05
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-12 19:25:54 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  while i < array.length
+    puts "#{i+1}. #{array[i]}"
+    i += 1
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0
+  a = []
+  while i < array.length
+    string = "#{array[i].capitalize}!"
+    a << string
+    i += 1
+  end
+  a
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array)
+  i = 0
+  while i < array.length
+    if array[i].length > 4
+      return true
+    else
+      i += 1
+    end
+  end
+    return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < array.length
+    if array[i] == cheese_types[0] || array[i] == cheese_types[1] || array[i] == cheese_types[2]
+      return array[i]
+    else
+      i += 1
+    end
+  end
 end
